dac9dae819214c26e7e67f82491dec6d1a235990
David Mogbeyi <mogbeyidavid@gmail.com>
2019-12-19 21:51:12 +0100

    Templated the auth module

---
+++ app/Repositories/Contracts/UserInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidmogbeyiteren
+ * Date: 2019-12-19
+ * Time: 21:47
+ */
+
+namespace App\Repositories\Contracts;
+
+
+interface UserInterface
+{
+
+}
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: davidmogbeyiteren
+ * Date: 2019-12-19
+ * Time: 21:46
+ */
+
+namespace App\Repositories;
+
+
+class UserRepository
+{
+
+}
