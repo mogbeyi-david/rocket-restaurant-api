81363aa4b786a1e6a58a60f3eab9dd4e470e0a84
David Mogbeyi <mogbeyidavid@gmail.com>
2019-12-19 21:18:16 +0100

    Implemented the users seeder class

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
+            $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedInteger('role_id');
+            $table->foreign('role_id')->references('role')->on('roles');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
